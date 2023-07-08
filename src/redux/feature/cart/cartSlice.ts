2f27d9acfd6b73cd3af94990dc842cb75ffa4432
EmranSWE <mdemran.swe@gmail.com>
2023-07-08 21:43:33 +0600

    completing cart feature

--- src/redux/feature/cart/cartSlice.ts
+++ src/redux/feature/cart/cartSlice.ts
@@ -1,4 +1,5 @@
-import { IProduct } from '@/types/globalTypes';
+import { IProduct } from './../../../types/globalTypes';
+
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 interface ICart {
   products: IProduct[];
@@ -20,7 +21,24 @@ const cartSlice = createSlice({
         state.products.push({ ...action.payload, quantity: 1 });
       }
     },
+    removeOne: (state, action: PayloadAction<IProduct>) => {
+      const existing = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+      if (existing && existing.quantity! > 1) {
+        existing.quantity = existing.quantity! - 1;
+      } else {
+        state.products = state.products.filter(
+          (product) => product._id !== action.payload._id
+        );
+      }
+    },
+    removeFromCart: (state, action: PayloadAction<IProduct>) => {
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload._id
+      );
+    },
   },
 });
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, removeOne } = cartSlice.actions;
 export default cartSlice.reducer;
