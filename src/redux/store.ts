e2ace6ba326ca9d97c31fa292180587f0081e01f
EmranSWE <mdemran.swe@gmail.com>
2023-07-10 23:30:13 +0600

    product filters using redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './feature/cart/cartSlice';
+import productSlice from './feature/products/productSlice';
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    product: productSlice,
   },
 });
 
